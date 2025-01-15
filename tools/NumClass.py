781d824412d49c12cce859ba3b4b12a2a10f5f0b
Griffin Finkler <ermavillage@gmail.com>
2025-01-15 10:47:23 -0500

    Overload_NumClass.py
    Added new overloads almost matching normal functionality of math

--- tools/NumClass.py
+++ tools/NumClass.py
@@ -4,7 +4,7 @@ import numpy as np
 ##watch out for divide by zero errors
 class Num:
     def __init__(self, op1):
-        self.out = op1
+        self.out = np.longdouble(op1)
     def __add__(self, op2):
         if isinstance(op2, Num):
             res = np.add(self.out, op2.out)
@@ -40,3 +40,69 @@ class Num:
         else:
             res = np.power(self.out, op2)
             return Num(res)
+    def __floordiv__(self, op2):
+        if isinstance(op2, Num):
+            res = np.floor_divide(self.out, op2.out)
+            return Num(res)
+        else:
+            res = np.floor_divide(self.out, op2)
+            return Num(res)
+        
+    def __mod__(self, op2):
+        if isinstance(op2, Num):
+            res = np.mod(self.out, op2.out)
+            return Num(res)
+        else:
+            res = np.mod(self.out, op2)
+            return Num(res)
+
+    def __lt__(self, op2):
+        if isinstance(op2, Num):
+            res = np.less(self.out, op2.out)
+            return Num(res)
+        else:
+            res = np.less(self.out, op2)
+            return Num(res)
+    def __le__(self, op2):
+        if isinstance(op2, Num):
+            res = np.less_equal(self.out, op2.out)
+            return Num(res)
+        else:
+            res = np.less_equal(self.out, op2)
+            return Num(res)
+    def __eq__(self, op2):
+        if isinstance(op2, Num):
+            res = np.equal(self.out, op2.out)
+            return Num(res)
+        else:
+            res = np.equal(self.out, op2)
+            return Num(res)
+    def __ne__(self, op2):
+        if isinstance(op2, Num):
+            res = np.not_equal(self.out, op2.out)
+            return Num(res)
+        else:
+            res = np.not_equal(self.out, op2)
+            return Num(res)
+    def __gt__(self, op2):
+        if isinstance(op2, Num):
+            res = np.greater(self.out, op2.out)
+            return Num(res)
+        else:
+            res = np.greater(self.out, op2)
+            return Num(res)
+    def __ge__(self, op2):
+        if isinstance(op2, Num):
+            res = np.greater_equal(self.out, op2.out)
+            return Num(res)
+        else:
+            res = np.greater_equal(self.out, op2)
+            return Num(res)
+    def __neg__(self):
+        res = np.negative(self.out)
+        return Num(res)
+    def __abs__(self):
+        res = np.absolute(self.out)
+        return Num(res)
+    def __str__(self):
+        return str(self.out)
