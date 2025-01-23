4cef856e461294363a5a49dc7fd517acd41e3153
Griffin Finkler <ermavillage@gmail.com>
2025-01-23 14:22:27 -0500

    Added tests for math that all pass and updated Numclas with simple error raises

--- tools/inertiaEq.py
+++ tools/inertiaEq.py
@@ -4,45 +4,61 @@ import time as tm
 
 ##STATIC Globals
 TRANSMISSIONEFFICIENCY = Num(0.9)
-TIREDIAMETER = Num(0.3) ##m
-GEARINGRATIO = Num(1.0) ##tire rev / motor rev
-ROLLINGRESISTANCE = Num(1.0) ##Nm
-MASS = Num(100.0) ##kg
-GRAVACCELLERATIONCONST = Num(9.8) ##m/s^2
-TIREPRESSURE = Num(2.0) ##barr
-DRAGCOEFF = Num(0.8) ##unitless
-MAXCROSSSECTIONALAREA = Num(0.5) ##m^2
-AIRDENSITY = Num(1.2) ##kg/m^3
+TIREDIAMETER = Num(0.3)  ##m
+GEARINGRATIO = Num(1.0)  ##tire rev / motor rev
+ROLLINGRESISTANCE = Num(1.0)  ##Nm
+MASS = Num(100.0)  ##kg
+GRAVACCELLERATIONCONST = Num(9.8)  ##m/s^2
+TIREPRESSURE = Num(2.0)  ##barr
+DRAGCOEFF = Num(0.8)  ##unitless
+MAXCROSSSECTIONALAREA = Num(0.5)  ##m^2
+AIRDENSITY = Num(1.2)  ##kg/m^3
 
 ##DYNAMIC Globals
-MOTORSPEED = Num(21.220659078940496) ##rev/s for vel = 20m/s
+MOTORSPEED = Num(21.220659078940496)  ##rev/s for vel = 20m/s
 
 ##PREVIOUS Globals
-V_PREV = Num(0.0) ##dv
-T_PREV = Num(tm.time()) ##dt in seconds
+V_PREV = Num(0.0)  ##dv
+T_PREV = Num(tm.time())  ##dt in seconds
+
 
 def rpm_to_motorspeed(rpm):
     rpmNum = Num(rpm)
-    return (rpmNum / Num(60))
+    return rpmNum / Num(60)
 
-def velocity(motorspeed): ##probable inputs
+
+def velocity(motorspeed):  ##probable inputs
     return GEARINGRATIO * motorspeed * TIREDIAMETER * Num(np.pi)
+
 
 def dVdT(velocity):
     global T_PREV
-    global V_PREV    
+    global V_PREV
 
-    time = Num(tm.time()) #time in seconds
+    time = Num(tm.time())  # time in seconds
     dV = velocity - V_PREV
-    dT  = time - T_PREV
-    #print("DT: ", dT)
+    dT = time - T_PREV
+    # print("DT: ", dT)
     V_PREV = velocity
     T_PREV = time
-    return (dV / dT) ##returns the difference in speed over small time step in seconds
+    return dV / dT  ##returns the difference in speed over small time step in seconds
 
-def acceleration_torque(motorspeed): ##probable inputs
-    c = Num(0.005) + ((Num(1) / TIREPRESSURE) * (Num(0.01) + Num(0.0095) * ((Num(3.6) * velocity(motorspeed))/ Num(100))**Num(2)))
+
+def acceleration_torque(motorspeed):  ##probable inputs
+    c = Num(0.005) + (
+        (Num(1) / TIREPRESSURE)
+        * (
+            Num(0.01)
+            + Num(0.0095) * ((Num(3.6) * velocity(motorspeed)) / Num(100)) ** Num(2)
+        )
+    )
     fR = c * MASS * GRAVACCELLERATIONCONST
-    fD = Num(0.5) * DRAGCOEFF * MAXCROSSSECTIONALAREA * AIRDENSITY * (velocity(motorspeed) ** Num(2))
+    fD = (
+        Num(0.5)
+        * DRAGCOEFF
+        * MAXCROSSSECTIONALAREA
+        * AIRDENSITY
+        * (velocity(motorspeed) ** Num(2))
+    )
     fI = MASS * dVdT(velocity(motorspeed))
-    return (fR + fD + fI) #* velocity(motorspeed) ##watts
+    return fR + fD + fI  # * velocity(motorspeed) ##watts
--- tools/inertiaEqTests.py
+++ tools/inertiaEqTests.py
@@ -1,2 +1,102 @@
-import inertiaEq
+from NumClass import Num
+import numpy as np
+#import inertiaEq
+import unittest
+import math
 
+##STATIC Globals
+TRANSMISSIONEFFICIENCY = Num(0.9)
+TIREDIAMETER = Num(0.3) ##m
+GEARINGRATIO = Num(1.0) ##tire rev / motor rev
+ROLLINGRESISTANCE = Num(1.0) ##Nm
+MASS = Num(100.0) ##kg
+GRAVACCELLERATIONCONST = Num(9.8) ##m/s^2
+TIREPRESSURE = Num(2.0) ##barr
+DRAGCOEFF = Num(0.8) ##unitless
+MAXCROSSSECTIONALAREA = Num(0.5) ##m^2
+AIRDENSITY = Num(1.2) ##kg/m^3
+
+
+
+def round_down_6_decimals(number):
+    factor = 10**6  # Shift decimal point 6 places
+    return math.floor(number * factor) / factor
+
+def v_mo_conversion(velocity):
+    s = Num(1)/((GEARINGRATIO * TIREDIAMETER * np.pi) / velocity) 
+    return s
+
+def rpm_to_motorspeed(rpm):
+    rpmNum = Num(rpm)
+    return (rpmNum / Num(60))
+
+def velocity(motorspeed): ##probable inputs
+    return GEARINGRATIO * motorspeed * TIREDIAMETER * Num(np.pi)
+
+def dVdT(velocity):
+    return 0 #(dV / dT) ##non important for tests
+
+def acceleration_torque(motorspeed): ##probable inputs
+    c = Num(0.005) + ((Num(1) / TIREPRESSURE) * (Num(0.01) + Num(0.0095) * ((Num(3.6) * velocity(motorspeed))/ Num(100))**Num(2)))
+    fR = c * MASS * GRAVACCELLERATIONCONST
+    fD = Num(0.5) * DRAGCOEFF * MAXCROSSSECTIONALAREA * AIRDENSITY * (velocity(motorspeed) ** Num(2))
+    fI = MASS * dVdT(velocity(motorspeed))
+    return (fR + fD + fI) #* velocity(motorspeed) ##watts
+
+print("vel to mtspd: ", v_mo_conversion(Num(10)))
+#print("Accelleration torque: ", acceleration_torque(v_mo_conversion('hello')))
+
+class TestAccellerationTorqueFunc(unittest.TestCase):
+    def test_accelleration_torqueAt20(self):
+        inputVelocity = Num(20)
+        expectedOutput = Num(108.213152).out
+        velocity = acceleration_torque(v_mo_conversion(inputVelocity)).out
+        velocitySm = round_down_6_decimals(velocity)
+        self.assertEqual(velocitySm, expectedOutput)
+
+    def test_accelleration_torqueAt10(self):
+        inputVelocity = Num(10)
+        expectedOutput = Num(34.403288).out
+        velocity = acceleration_torque(v_mo_conversion(inputVelocity)).out
+        velocitySm = round_down_6_decimals(velocity)
+        self.assertEqual(velocitySm, expectedOutput)
+
+    def test_accelleration_torqueAt1(self):
+        inputVelocity = Num(1)
+        expectedOutput = Num(10.046032).out
+        velocity = acceleration_torque(v_mo_conversion(inputVelocity)).out
+        velocitySm = round_down_6_decimals(velocity)
+        self.assertEqual(velocitySm, expectedOutput)
+
+    def test_accelleration_torqueAtNeg1(self):
+        inputVelocity = Num(-1)
+        expectedOutput = Num(10.046032).out
+        velocity = acceleration_torque(v_mo_conversion(inputVelocity)).out
+        velocitySm = round_down_6_decimals(velocity)
+        self.assertEqual(velocitySm, expectedOutput)
+
+    def test_accelleration_torqueAtNeg10(self):
+        inputVelocity = Num(-10)
+        expectedOutput = Num(34.403288).out
+        velocity = acceleration_torque(v_mo_conversion(inputVelocity)).out
+        velocitySm = round_down_6_decimals(velocity)
+        self.assertEqual(velocitySm, expectedOutput)
+
+    def test_accelleration_torqueAtNeg20(self):
+        inputVelocity = Num(-20)
+        expectedOutput = Num(108.213152).out
+        velocity = acceleration_torque(v_mo_conversion(inputVelocity)).out
+        velocitySm = round_down_6_decimals(velocity)
+        self.assertEqual(velocitySm, expectedOutput)
+
+    def test_accelleration_torqueWONumClass(self):
+        inputVelocity = 20
+        expectedOutput = Num(108.213152).out
+        velocity = acceleration_torque(v_mo_conversion(inputVelocity)).out
+        velocitySm = round_down_6_decimals(velocity)
+        self.assertEqual(velocitySm, expectedOutput)
+    
+
+    
+
+unittest.main()
