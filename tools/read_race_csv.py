f519838ac7c12d924741054d68530b6d847f3473
davisgc <davisgc@uindy.edu>
2025-02-04 14:19:02 -0500

    Update read_race_csv.py
    added notes for what needs to be done to get it in a working state.

--- tools/read_race_csv.py
+++ tools/read_race_csv.py
@@ -19,6 +19,13 @@ def read_race_data(standardized = False):
     return data
 
 if __name__ == '__main__':
-    data = read_race_data(standardized=True)
+    # i don't know what metric we can read from the csv just yet. rpm is a placeholder.
+    # we may need to do math that allows us to know what duty cycle we need to apply to reach the desired rpm.
+    # we need to take speed, tire diameter, rpm. circumference is pi times diamater. multiply by rps to get velocity.
+    # We know we can't go further than 12.824 m/s across the turn or we will slip.
+    # if we can correlate duty cycle to the speeds we need to hit, we can create a function to feed the right duty cycle to the controller.
+    # We can divide the desired rps by the max of the motor to find what duty cycle we may need, but we also need to account for the braking
+    #     we need to overcome to reach the speeds. This may not be required depending on how the dyno works.
+    data = read_race_data()
     for row in data:
         print(f"time: {row[0]}, rpm: {row[1]}")
