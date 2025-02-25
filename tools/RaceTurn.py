fe65d304e6e58f3b258c81a566cf8efdac5e1e6b
davisgc <davisgc@uindy.edu>
2025-02-25 13:26:57 -0500

    Update RaceTurn.py

--- tools/RaceTurn.py
+++ tools/RaceTurn.py
@@ -1,7 +1,17 @@
+import math
+STATICFRICTION = 2.480847866
+GRAV_ACCELLERATION = 9.8
+
 class RaceTurn:
-    def __init__(self, turn=False, length=0, max_speed=-1):
-        self.turn = turn
-        self.length = length
-        self.max_speed = max_speed
-        if not turn:
-            max_speed = -1
+    def __init__(self, length, turn_radius=-1, max_speed = -1):
+        self.length = abs(length)
+        self.turn_radius = turn_radius
+        if turn_radius > 0:
+            self.calcMaxSpeed(turn_radius)
+        
+    def calcMaxSpeed(self, turn_radius):
+        self.max_speed = math.sqrt(STATICFRICTION * GRAV_ACCELLERATION * turn_radius)
+
+if __name__ == '__main__':
+    TestTurn = RaceTurn(length=10, turn_radius = 7.62)
+    print(TestTurn.max_speed)
