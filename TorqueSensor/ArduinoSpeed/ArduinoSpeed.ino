8ef6210ad30f5d7c5c9ae42b56296f95395d3386
reyessanchezo <90915894+reyessanchezo@users.noreply.github.com>
2025-03-11 14:50:27 -0400

    Update ArduinoSpeed.ino

--- TorqueSensor/ArduinoSpeed/ArduinoSpeed.ino
+++ TorqueSensor/ArduinoSpeed/ArduinoSpeed.ino
@@ -1,31 +1,46 @@
-const byte encoderPin = 2;       // Encoder output connected to digital pin 2
+const byte encoderPin = 3;       // Encoder output connected to digital pin 3
+const byte torquePin = A4;       // Torque output connected to Analog pin 4
 volatile unsigned long pulseCount = 0;
 unsigned long previousMillis = 0;
-const unsigned long interval = 1000; // Interval for RPM calculation (1 second)
-const unsigned int pulsesPerRevolution = 60;
+const float interval = 10.0; // Output interval (milliseconds)
+const float pulsesPerRevolution = 60.0;
+const float intervalPerMinute = 60000.0 / interval;
 const byte ledPin = 13;
+float rpm = 0.0;
+float torque = 0.0;
+int actualInterval = 0;
+unsigned long count = 0;
+unsigned long long currentMillis = 0;
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   pinMode(encoderPin, INPUT_PULLUP);
+  pinMode(torquePin, INPUT);
   pinMode(ledPin, OUTPUT);
   attachInterrupt(digitalPinToInterrupt(encoderPin), countPulse, RISING);
 }
 
 void loop() {
-  unsigned long currentMillis = millis();
+  currentMillis = millis();
   if (currentMillis - previousMillis >= interval) {
     noInterrupts(); // Temporarily disable interrupts for accurate count
-    unsigned long count = pulseCount;
+    count = pulseCount;
+    currentMillis = millis();
     pulseCount = 0;
     interrupts(); // Re-enable interrupts
-    previousMillis = currentMillis;
+    actualInterval = currentMillis - previousMillis;
+    previousMillis = currentMillis;  
     
-    // Calculate RPM
-    float rpm = count / pulsesPerRevolution;
+    
+    // Calculate values
+    torque = ((analogRead(torquePin) * 60.0) / 1023.0) - 30.0;
+    rpm = count * (1000/actualInterval) * 60 / pulsesPerRevolution / 2;
     Serial.print("RPM: ");
-    Serial.println(rpm);
+    Serial.print(rpm, 1);
+    Serial.print(" Torque(Nm): ");
+    Serial.println(torque, 2);
   }
+  
 }
 
 void countPulse() {
