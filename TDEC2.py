2471d2ed50d7c3a3b1b629c44c5c3478b531d559
Griffin Finkler <ermavillage@gmail.com>
2025-03-24 16:04:07 -0400

    fixed globals and did minimal cleanup

--- TDEC2.py
+++ TDEC2.py
@@ -11,20 +11,21 @@ from queue import Queue, Empty
 from tools import choose_port
 import time
 
-STATICFRICTION = 2.480847866
+STATICFRICTIONSIDELOADING = 1.8 
+STATICFRICTION = 0.8 #2.480847866
 NUM_LAPS = 1
 POLLING_RATE = 0.1
 
 AIR_DENSITY = 1.2  # Air density (kg/m^3)
 DRAG_COEFF = 0.8  # Drag Coefficient (unitless)
 GRAV_ACCELLERATION = 9.8  # Gravity acceleration constant (m/s^2)
-GEARING_RATIO = 13.0 / 55.0  # Gearing Ratio (Tire revolutions / Motor revolutions)
-MASS = 100.0  # Kart mass (kg)
+GEARING_RATIO = 9.0 / 68.0  # Gearing Ratio (Tire revolutions / Motor revolutions)
+MASS = 136.0  # Kart mass (kg)
 MAX_CROSSSECTIONAL_AREA = 0.5  # Maximum cross-sectional area (m^2)
 TIRE_DIAMETER = 0.254  # Kart tire diameter (m)
-TIRE_PRESSURE = 2.0  # Tire pressure (barr)
+TIRE_PRESSURE = 1.0  # Tire pressure (barr)
 TRANSMISSION_EFFICIENCY = 0.9  # Need clarification!!!
-ROLLING_RESISTANCE = 1.0  # Nm. Need clarification!!!
+#ROLLING_RESISTANCE = 1.0  # Nm. Need clarification!!!
 
 MOTORTORQUE = 3.7 * 3  ##Nm
 
@@ -39,7 +40,7 @@ class RaceSeg:
             self.maxRPM = 4500
         
     def calcMaxSpeed(self, turnRadius):
-        self.maxSpeed = math.sqrt(STATICFRICTION * GRAV_ACCELLERATION * turnRadius)
+        self.maxSpeed = math.sqrt(STATICFRICTIONSIDELOADING * GRAV_ACCELLERATION * turnRadius)
         self.maxRPM = self.maxSpeed / (math.pi * TIRE_DIAMETER * GEARING_RATIO) * 60
 
 class RaceInfo:
