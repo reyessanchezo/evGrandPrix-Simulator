b8966f05c90115b4812dbc652b52acde3fd7b8c0
Griffin Finkler <ermavillage@gmail.com>
2025-04-01 13:55:12 -0400

    added a reference point for math

---
+++ updatedMathVariables.py
@@ -0,0 +1,17 @@
+STATICFRICTIONSIDELOADING = 0.4
+STATICFRICTION = 0.4 #2.480847866
+NUM_LAPS = 3
+POLLING_RATE = 0.1
+
+AIR_DENSITY = 1.2  # Air density (kg/m^3)
+DRAG_COEFF = 0.8  # Drag Coefficient (unitless)
+GRAV_ACCELLERATION = 9.8  # Gravity acceleration constant (m/s^2)
+GEARING_RATIO = 9.0 / 68.0  # Gearing Ratio (Tire revolutions / Motor revolutions)
+MASS = 136.0  # Kart mass (kg)
+MAX_CROSSSECTIONAL_AREA = 0.5  # Maximum cross-sectional area (m^2)
+TIRE_DIAMETER = 0.254  # Kart tire diameter (m)
+TIRE_PRESSURE = 1.0  # Tire pressure (barr)
+TRANSMISSION_EFFICIENCY = 0.9  # Need clarification!!!
+ROLLING_RESISTANCE = 1.0  # Nm. Need clarification!!!
+
+MOTORTORQUE = 3.7 * 3  ##Nm
